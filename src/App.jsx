474071f2485dfad9489d124a5bc2821e418bcb93
Dhiraj <thedhirajofficials@gmail.com>
2026-01-16 00:40:16 +0530

    Fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './component/Header'
 import Hero from './component/Hero'
-import Services from './component/Services'
-import Footer from './component/Footer'
+
 
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
     <>
       <Header></Header>
       <Hero></Hero>
-      <Footer></Footer>
+     
       
       
     </>
